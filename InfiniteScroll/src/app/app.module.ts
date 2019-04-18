399bf30ab47abca1392decf6f783c21ff18d80bc
Niran <Niran.Vijayakumar@ust-global.com>
2019-04-18 12:35:39 +0530

    Added Routes

--- InfiniteScroll/src/app/app.module.ts
+++ InfiniteScroll/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgModule } from '@angular/core';
-import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,27 +12,41 @@ import { MatInputModule } from '@angular/material';
 import { MatFormFieldModule } from '@angular/material';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { AngularmaterialModule } from './material/angularmaterial/angularmaterial.module';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { Routes, RouterModule } from '@angular/router';
 
+const appRoutes: Routes=[
+  {path:'login',component: LoginComponent},
+  {path:'home',component: HomeComponent},
+  {path:'users',component: UserComponent},
+];
 
 @NgModule({
   declarations: [
-  AppComponent,
-  UserComponent
+    AppComponent,
+    UserComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-  BrowserModule,
-  HttpClientModule,
-  InfiniteScrollModule,
-  MatFormFieldModule,
-  MatInputModule,
-  AppRoutingModule,
-  BrowserAnimationsModule,
-  AngularmaterialModule,
-  FormsModule,
-  ReactiveFormsModule,
+    BrowserModule,
+    HttpClientModule,
+    InfiniteScrollModule,
+    MatFormFieldModule,
+    MatInputModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularmaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
 
   providers: [],
   bootstrap: [AppComponent]
-  })
-  export class AppModule { }
+})
+export class AppModule { }
