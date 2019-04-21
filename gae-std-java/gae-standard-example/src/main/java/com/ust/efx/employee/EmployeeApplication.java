25233420b45c85f126541498a621ed33a78face4
Niran <niranvv@gmail.com>
2019-04-21 10:46:06 +0530

    Renamed GAE Demo application to Employee App

--- gae-std-java/gae-standard-example/src/main/java/com/ust/efx/employee/EmployeeApplication.java
+++ gae-std-java/gae-standard-example/src/main/java/com/ust/efx/employee/EmployeeApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.ust.efx.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class EmployeeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(EmployeeApplication.class, args);
 	}
 	
   @GetMapping("/")
