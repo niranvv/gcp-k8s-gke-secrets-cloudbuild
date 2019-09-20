c8fdb58c2ec4f33e54d67ffea73ea6b3103ef0e2
Niran <niranvv@gmail.com>
2019-09-20 09:18:23 +0530

    Added HomeController & Readme

---
+++ java_simple_rest_api/readme.md
@@ -0,0 +1,6 @@
+# Commands
+* mvn compile
+* mvn package
+* ./mvnw clean package or mvn clean package
+* ./mvnw spring-boot:run or mvn spring-boot:run
+* java -jar target/java-api-0.1.0.jar
---
+++ java_simple_rest_api/src/main/java/hello/HomeController.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Call '/greeting' endpoint with query parameter 'name'";
+    }
+}
